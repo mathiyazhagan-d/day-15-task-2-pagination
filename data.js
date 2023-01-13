1dbbe830ec5c74efa3061a58fb1839da6b583789
mathiyazhagan-d <116990455+mathiyazhagan-d@users.noreply.github.com>
2023-01-13 15:08:59 +0530

    form

--- data.js
+++ data.js
@@ -1,504 +1,504 @@
-const data=[
-{
-"name": "Miss Flora Spinka",
-"email": "Sheridan63@example.org",
-"id": "1"
-},
-{
-"name": "Sherri Hudson",
-"email": "Demond54@example.net",
-"id": "2"
-},
-{
-"name": "Glenn Metz",
-"email": "Gayle_Kerluke25@example.net",
-"id": "3"
-},
-{
-"name": "Beatrice Breitenberg Jr.",
-"email": "Jonas73@example.net",
-"id": "4"
-},
-{
-"name": "Domingo Roberts",
-"email": "Ward_Brown40@example.net",
-"id": "5"
-},
-{
-"name": "Lucas Hammes",
-"email": "Tevin13@example.com",
-"id": "6"
-},
-{
-"name": "Janis Zboncak",
-"email": "Hailey.Mraz0@example.net",
-"id": "7"
-},
-{
-"name": "Monique Goyette IV",
-"email": "Nayeli88@example.net",
-"id": "8"
-},
-{
-"name": "Dr. Stewart Spencer",
-"email": "Crystel.Thompson97@example.com",
-"id": "9"
-},
-{
-"name": "Bill Funk",
-"email": "Lorenzo_Leannon72@example.org",
-"id": "10"
-},
-{
-"name": "Norma Bartell",
-"email": "Abby26@example.org",
-"id": "11"
-},
-{
-"name": "Cary Ledner",
-"email": "Rosanna.Cole@example.com",
-"id": "12"
-},
-{
-"name": "Shane Pagac",
-"email": "Carroll_Boehm64@example.net",
-"id": "13"
-},
-{
-"name": "Sam Monahan",
-"email": "Cassandra.Will@example.net",
-"id": "14"
-},
-{
-"name": "Javier Mraz",
-"email": "Gabriella_OHara@example.com",
-"id": "15"
-},
-{
-"name": "Gladys Hand",
-"email": "Madie.Mayert@example.net",
-"id": "16"
-},
-{
-"name": "Raquel Brekke",
-"email": "Delaney_Renner19@example.com",
-"id": "17"
-},
-{
-"name": "Jamie Kohler",
-"email": "Woodrow31@example.com",
-"id": "18"
-},
-{
-"name": "Nadine Lesch",
-"email": "Friedrich.Feil98@example.org",
-"id": "19"
-},
-{
-"name": "Mr. Ginger Dooley",
-"email": "Brook.Runolfsdottir@example.com",
-"id": "20"
-},
-{
-"name": "Tim Kuhlman",
-"email": "Omari37@example.net",
-"id": "21"
-},
-{
-"name": "Darrel Hayes",
-"email": "Merl.Cronin61@example.org",
-"id": "22"
-},
-{
-"name": "Miss Raul Lind",
-"email": "Dannie.Glover42@example.net",
-"id": "23"
-},
-{
-"name": "Tamara Stark Jr.",
-"email": "Fae_Dicki@example.net",
-"id": "24"
-},
-{
-"name": "Betty Jacobs",
-"email": "Jayda_Reichert@example.net",
-"id": "25"
-},
-{
-"name": "Carole Daugherty",
-"email": "Elda.Oberbrunner76@example.org",
-"id": "26"
-},
-{
-"name": "Brett McLaughlin",
-"email": "Enid_McGlynn@example.com",
-"id": "27"
-},
-{
-"name": "Jon Blanda",
-"email": "Gardner.Lynch85@example.org",
-"id": "28"
-},
-{
-"name": "Gilberto Adams",
-"email": "Leora67@example.com",
-"id": "29"
-},
-{
-"name": "Elmer Kovacek II",
-"email": "Danyka11@example.net",
-"id": "30"
-},
-{
-"name": "Brittany Koss",
-"email": "Rhett89@example.net",
-"id": "31"
-},
-{
-"name": "Ms. Nicolas Goldner",
-"email": "Birdie_Jaskolski@example.org",
-"id": "32"
-},
-{
-"name": "Kathleen Corwin",
-"email": "Nathen_Macejkovic@example.org",
-"id": "33"
-},
-{
-"name": "Hannah Dare",
-"email": "Alek.Marks23@example.org",
-"id": "34"
-},
-{
-"name": "Jan Swaniawski",
-"email": "Orval21@example.net",
-"id": "35"
-},
-{
-"name": "Leticia Haley",
-"email": "Gillian_Waters@example.org",
-"id": "36"
-},
-{
-"name": "Antoinette Tromp",
-"email": "Austyn_Dach60@example.net",
-"id": "37"
-},
-{
-"name": "Grace Cronin",
-"email": "Carolanne.Langosh@example.com",
-"id": "38"
-},
-{
-"name": "Larry Hermann",
-"email": "Brianne.Beahan48@example.com",
-"id": "39"
-},
-{
-"name": "Ms. Milton Schneider",
-"email": "Amya0@example.net",
-"id": "40"
-},
-{
-"name": "Antonio Witting",
-"email": "Mortimer21@example.org",
-"id": "41"
-},
-{
-"name": "Deborah Walker",
-"email": "Kallie50@example.net",
-"id": "42"
-},
-{
-"name": "Mrs. Marlene McLaughlin",
-"email": "Forrest_Deckow@example.net",
-"id": "43"
-},
-{
-"name": "Mr. Delbert Batz",
-"email": "Lawrence_West@example.net",
-"id": "44"
-},
-{
-"name": "Rosie Stanton",
-"email": "Mariela.Rippin@example.net",
-"id": "45"
-},
-{
-"name": "Aubrey Schmitt",
-"email": "Boyd.Hahn28@example.net",
-"id": "46"
-},
-{
-"name": "Winston Conn",
-"email": "Zander.Raynor@example.net",
-"id": "47"
-},
-{
-"name": "Mrs. Neil Brekke",
-"email": "Charles.Abshire21@example.org",
-"id": "48"
-},
-{
-"name": "Frances Schoen",
-"email": "Abbie.Kozey40@example.org",
-"id": "49"
-},
-{
-"name": "Ricky Carroll",
-"email": "Rosalia.Treutel41@example.com",
-"id": "50"
-},
-{
-"name": "Al Buckridge",
-"email": "Elda42@example.com",
-"id": "51"
-},
-{
-"name": "Virginia Ritchie",
-"email": "Albina.Hickle@example.org",
-"id": "52"
-},
-{
-"name": "Dixie Jacobi",
-"email": "Eldridge77@example.com",
-"id": "53"
-},
-{
-"name": "Holly Ruecker",
-"email": "Gillian_Balistreri@example.com",
-"id": "54"
-},
-{
-"name": "Michael Franey",
-"email": "Alessandra10@example.net",
-"id": "55"
-},
-{
-"name": "Sarah Feil",
-"email": "June16@example.net",
-"id": "56"
-},
-{
-"name": "Ms. Chester Wolf",
-"email": "Rick_Gibson@example.com",
-"id": "57"
-},
-{
-"name": "Erin Fahey",
-"email": "Verner30@example.com",
-"id": "58"
-},
-{
-"name": "Shirley Deckow I",
-"email": "Rosie87@example.net",
-"id": "59"
-},
-{
-"name": "Rebecca King",
-"email": "Katrina_Gulgowski@example.org",
-"id": "60"
-},
-{
-"name": "Candice Gerlach",
-"email": "Elda78@example.org",
-"id": "61"
-},
-{
-"name": "Archie Kunze Sr.",
-"email": "Ken96@example.org",
-"id": "62"
-},
-{
-"name": "Shirley Spencer DVM",
-"email": "Tad41@example.com",
-"id": "63"
-},
-{
-"name": "Miss Clark Nikolaus",
-"email": "Arely59@example.net",
-"id": "64"
-},
-{
-"name": "Allison Schumm II",
-"email": "Demetrius_Orn66@example.net",
-"id": "65"
-},
-{
-"name": "David Maggio MD",
-"email": "Asha40@example.net",
-"id": "66"
-},
-{
-"name": "Ruben Waelchi",
-"email": "Damian_Boyle12@example.org",
-"id": "67"
-},
-{
-"name": "Willard Hickle II",
-"email": "Bria.Rempel@example.net",
-"id": "68"
-},
-{
-"name": "Leticia Johnston",
-"email": "Rebeka.Hoppe61@example.com",
-"id": "69"
-},
-{
-"name": "Dr. Jaime Abbott",
-"email": "Reilly_Predovic32@example.net",
-"id": "70"
-},
-{
-"name": "Alfonso Hilpert",
-"email": "Geo_Kuhic90@example.com",
-"id": "71"
-},
-{
-"name": "Eleanor Kuhn",
-"email": "Sasha51@example.com",
-"id": "72"
-},
-{
-"name": "Theresa Miller",
-"email": "Winnifred_Kautzer64@example.com",
-"id": "73"
-},
-{
-"name": "Ms. Helen Gerlach",
-"email": "Rudy.Monahan@example.org",
-"id": "74"
-},
-{
-"name": "Vicky Auer I",
-"email": "Suzanne_Wilkinson49@example.org",
-"id": "75"
-},
-{
-"name": "Ms. Leo Brown",
-"email": "Anjali_Kirlin83@example.org",
-"id": "76"
-},
-{
-"name": "Mrs. Bryan Ferry",
-"email": "Daren.Lind@example.net",
-"id": "77"
-},
-{
-"name": "Victoria Weimann",
-"email": "Brielle8@example.org",
-"id": "78"
-},
-{
-"name": "Darnell Stoltenberg",
-"email": "Nicholaus68@example.com",
-"id": "79"
-},
-{
-"name": "Mr. Margie Ratke",
-"email": "Mack.Upton0@example.net",
-"id": "80"
-},
-{
-"name": "Janet McKenzie",
-"email": "Troy.Lindgren65@example.org",
-"id": "81"
-},
-{
-"name": "Donald Gusikowski",
-"email": "Judy10@example.com",
-"id": "82"
-},
-{
-"name": "Doug Schneider",
-"email": "Julianne.Deckow34@example.net",
-"id": "83"
-},
-{
-"name": "Simon Hessel",
-"email": "Ottilie_Ebert10@example.com",
-"id": "84"
-},
-{
-"name": "Ervin Marvin",
-"email": "Catalina80@example.net",
-"id": "85"
-},
-{
-"name": "Perry Wisozk",
-"email": "Hailey.Littel@example.com",
-"id": "86"
-},
-{
-"name": "Richard Lueilwitz",
-"email": "Adriel_Cremin@example.com",
-"id": "87"
-},
-{
-"name": "Henrietta Hammes",
-"email": "Annabelle_Hickle50@example.com",
-"id": "88"
-},
-{
-"name": "Jeremiah Haley",
-"email": "Hans_Heaney30@example.org",
-"id": "89"
-},
-{
-"name": "Marian Hickle",
-"email": "Adalberto81@example.net",
-"id": "90"
-},
-{
-"name": "Lorenzo Cronin",
-"email": "Marcelina39@example.com",
-"id": "91"
-},
-{
-"name": "Ms. Jeremy Hodkiewicz",
-"email": "Ernestine.Reichel55@example.org",
-"id": "92"
-},
-{
-"name": "Leo Beatty",
-"email": "Wiley.Schumm67@example.com",
-"id": "93"
-},
-{
-"name": "Glen Casper",
-"email": "Chadrick.Jacobson@example.net",
-"id": "94"
-},
-{
-"name": "Eleanor Walter",
-"email": "Miracle.Bartell@example.com",
-"id": "95"
-},
-{
-"name": "Keith Pouros",
-"email": "Gracie_Hackett4@example.com",
-"id": "96"
-},
-{
-"name": "Alan Haag MD",
-"email": "Dorothea.Rosenbaum47@example.org",
-"id": "97"
-},
-{
-"name": "Miss Tracy Welch IV",
-"email": "Ashton_Weber@example.net",
-"id": "98"
-},
-{
-"name": "Ms. Robert Haley MD",
-"email": "Rey_Bailey63@example.org",
-"id": "99"
-},
-{
-"name": "Ellis Kling III",
-"email": "Ramon.Hayes@example.org",
-"id": "100"
-}
-]
+const data = [
+  {
+    name: "Miss Flora Spinka",
+    email: "Sheridan63@example.org",
+    id: "1",
+  },
+  {
+    name: "Sherri Hudson",
+    email: "Demond54@example.net",
+    id: "2",
+  },
+  {
+    name: "Glenn Metz",
+    email: "Gayle_Kerluke25@example.net",
+    id: "3",
+  },
+  {
+    name: "Beatrice Breitenberg Jr.",
+    email: "Jonas73@example.net",
+    id: "4",
+  },
+  {
+    name: "Domingo Roberts",
+    email: "Ward_Brown40@example.net",
+    id: "5",
+  },
+  {
+    name: "Lucas Hammes",
+    email: "Tevin13@example.com",
+    id: "6",
+  },
+  {
+    name: "Janis Zboncak",
+    email: "Hailey.Mraz0@example.net",
+    id: "7",
+  },
+  {
+    name: "Monique Goyette IV",
+    email: "Nayeli88@example.net",
+    id: "8",
+  },
+  {
+    name: "Dr. Stewart Spencer",
+    email: "Crystel.Thompson97@example.com",
+    id: "9",
+  },
+  {
+    name: "Bill Funk",
+    email: "Lorenzo_Leannon72@example.org",
+    id: "10",
+  },
+  {
+    name: "Norma Bartell",
+    email: "Abby26@example.org",
+    id: "11",
+  },
+  {
+    name: "Cary Ledner",
+    email: "Rosanna.Cole@example.com",
+    id: "12",
+  },
+  {
+    name: "Shane Pagac",
+    email: "Carroll_Boehm64@example.net",
+    id: "13",
+  },
+  {
+    name: "Sam Monahan",
+    email: "Cassandra.Will@example.net",
+    id: "14",
+  },
+  {
+    name: "Javier Mraz",
+    email: "Gabriella_OHara@example.com",
+    id: "15",
+  },
+  {
+    name: "Gladys Hand",
+    email: "Madie.Mayert@example.net",
+    id: "16",
+  },
+  {
+    name: "Raquel Brekke",
+    email: "Delaney_Renner19@example.com",
+    id: "17",
+  },
+  {
+    name: "Jamie Kohler",
+    email: "Woodrow31@example.com",
+    id: "18",
+  },
+  {
+    name: "Nadine Lesch",
+    email: "Friedrich.Feil98@example.org",
+    id: "19",
+  },
+  {
+    name: "Mr. Ginger Dooley",
+    email: "Brook.Runolfsdottir@example.com",
+    id: "20",
+  },
+  {
+    name: "Tim Kuhlman",
+    email: "Omari37@example.net",
+    id: "21",
+  },
+  {
+    name: "Darrel Hayes",
+    email: "Merl.Cronin61@example.org",
+    id: "22",
+  },
+  {
+    name: "Miss Raul Lind",
+    email: "Dannie.Glover42@example.net",
+    id: "23",
+  },
+  {
+    name: "Tamara Stark Jr.",
+    email: "Fae_Dicki@example.net",
+    id: "24",
+  },
+  {
+    name: "Betty Jacobs",
+    email: "Jayda_Reichert@example.net",
+    id: "25",
+  },
+  {
+    name: "Carole Daugherty",
+    email: "Elda.Oberbrunner76@example.org",
+    id: "26",
+  },
+  {
+    name: "Brett McLaughlin",
+    email: "Enid_McGlynn@example.com",
+    id: "27",
+  },
+  {
+    name: "Jon Blanda",
+    email: "Gardner.Lynch85@example.org",
+    id: "28",
+  },
+  {
+    name: "Gilberto Adams",
+    email: "Leora67@example.com",
+    id: "29",
+  },
+  {
+    name: "Elmer Kovacek II",
+    email: "Danyka11@example.net",
+    id: "30",
+  },
+  {
+    name: "Brittany Koss",
+    email: "Rhett89@example.net",
+    id: "31",
+  },
+  {
+    name: "Ms. Nicolas Goldner",
+    email: "Birdie_Jaskolski@example.org",
+    id: "32",
+  },
+  {
+    name: "Kathleen Corwin",
+    email: "Nathen_Macejkovic@example.org",
+    id: "33",
+  },
+  {
+    name: "Hannah Dare",
+    email: "Alek.Marks23@example.org",
+    id: "34",
+  },
+  {
+    name: "Jan Swaniawski",
+    email: "Orval21@example.net",
+    id: "35",
+  },
+  {
+    name: "Leticia Haley",
+    email: "Gillian_Waters@example.org",
+    id: "36",
+  },
+  {
+    name: "Antoinette Tromp",
+    email: "Austyn_Dach60@example.net",
+    id: "37",
+  },
+  {
+    name: "Grace Cronin",
+    email: "Carolanne.Langosh@example.com",
+    id: "38",
+  },
+  {
+    name: "Larry Hermann",
+    email: "Brianne.Beahan48@example.com",
+    id: "39",
+  },
+  {
+    name: "Ms. Milton Schneider",
+    email: "Amya0@example.net",
+    id: "40",
+  },
+  {
+    name: "Antonio Witting",
+    email: "Mortimer21@example.org",
+    id: "41",
+  },
+  {
+    name: "Deborah Walker",
+    email: "Kallie50@example.net",
+    id: "42",
+  },
+  {
+    name: "Mrs. Marlene McLaughlin",
+    email: "Forrest_Deckow@example.net",
+    id: "43",
+  },
+  {
+    name: "Mr. Delbert Batz",
+    email: "Lawrence_West@example.net",
+    id: "44",
+  },
+  {
+    name: "Rosie Stanton",
+    email: "Mariela.Rippin@example.net",
+    id: "45",
+  },
+  {
+    name: "Aubrey Schmitt",
+    email: "Boyd.Hahn28@example.net",
+    id: "46",
+  },
+  {
+    name: "Winston Conn",
+    email: "Zander.Raynor@example.net",
+    id: "47",
+  },
+  {
+    name: "Mrs. Neil Brekke",
+    email: "Charles.Abshire21@example.org",
+    id: "48",
+  },
+  {
+    name: "Frances Schoen",
+    email: "Abbie.Kozey40@example.org",
+    id: "49",
+  },
+  {
+    name: "Ricky Carroll",
+    email: "Rosalia.Treutel41@example.com",
+    id: "50",
+  },
+  {
+    name: "Al Buckridge",
+    email: "Elda42@example.com",
+    id: "51",
+  },
+  {
+    name: "Virginia Ritchie",
+    email: "Albina.Hickle@example.org",
+    id: "52",
+  },
+  {
+    name: "Dixie Jacobi",
+    email: "Eldridge77@example.com",
+    id: "53",
+  },
+  {
+    name: "Holly Ruecker",
+    email: "Gillian_Balistreri@example.com",
+    id: "54",
+  },
+  {
+    name: "Michael Franey",
+    email: "Alessandra10@example.net",
+    id: "55",
+  },
+  {
+    name: "Sarah Feil",
+    email: "June16@example.net",
+    id: "56",
+  },
+  {
+    name: "Ms. Chester Wolf",
+    email: "Rick_Gibson@example.com",
+    id: "57",
+  },
+  {
+    name: "Erin Fahey",
+    email: "Verner30@example.com",
+    id: "58",
+  },
+  {
+    name: "Shirley Deckow I",
+    email: "Rosie87@example.net",
+    id: "59",
+  },
+  {
+    name: "Rebecca King",
+    email: "Katrina_Gulgowski@example.org",
+    id: "60",
+  },
+  {
+    name: "Candice Gerlach",
+    email: "Elda78@example.org",
+    id: "61",
+  },
+  {
+    name: "Archie Kunze Sr.",
+    email: "Ken96@example.org",
+    id: "62",
+  },
+  {
+    name: "Shirley Spencer DVM",
+    email: "Tad41@example.com",
+    id: "63",
+  },
+  {
+    name: "Miss Clark Nikolaus",
+    email: "Arely59@example.net",
+    id: "64",
+  },
+  {
+    name: "Allison Schumm II",
+    email: "Demetrius_Orn66@example.net",
+    id: "65",
+  },
+  {
+    name: "David Maggio MD",
+    email: "Asha40@example.net",
+    id: "66",
+  },
+  {
+    name: "Ruben Waelchi",
+    email: "Damian_Boyle12@example.org",
+    id: "67",
+  },
+  {
+    name: "Willard Hickle II",
+    email: "Bria.Rempel@example.net",
+    id: "68",
+  },
+  {
+    name: "Leticia Johnston",
+    email: "Rebeka.Hoppe61@example.com",
+    id: "69",
+  },
+  {
+    name: "Dr. Jaime Abbott",
+    email: "Reilly_Predovic32@example.net",
+    id: "70",
+  },
+  {
+    name: "Alfonso Hilpert",
+    email: "Geo_Kuhic90@example.com",
+    id: "71",
+  },
+  {
+    name: "Eleanor Kuhn",
+    email: "Sasha51@example.com",
+    id: "72",
+  },
+  {
+    name: "Theresa Miller",
+    email: "Winnifred_Kautzer64@example.com",
+    id: "73",
+  },
+  {
+    name: "Ms. Helen Gerlach",
+    email: "Rudy.Monahan@example.org",
+    id: "74",
+  },
+  {
+    name: "Vicky Auer I",
+    email: "Suzanne_Wilkinson49@example.org",
+    id: "75",
+  },
+  {
+    name: "Ms. Leo Brown",
+    email: "Anjali_Kirlin83@example.org",
+    id: "76",
+  },
+  {
+    name: "Mrs. Bryan Ferry",
+    email: "Daren.Lind@example.net",
+    id: "77",
+  },
+  {
+    name: "Victoria Weimann",
+    email: "Brielle8@example.org",
+    id: "78",
+  },
+  {
+    name: "Darnell Stoltenberg",
+    email: "Nicholaus68@example.com",
+    id: "79",
+  },
+  {
+    name: "Mr. Margie Ratke",
+    email: "Mack.Upton0@example.net",
+    id: "80",
+  },
+  {
+    name: "Janet McKenzie",
+    email: "Troy.Lindgren65@example.org",
+    id: "81",
+  },
+  {
+    name: "Donald Gusikowski",
+    email: "Judy10@example.com",
+    id: "82",
+  },
+  {
+    name: "Doug Schneider",
+    email: "Julianne.Deckow34@example.net",
+    id: "83",
+  },
+  {
+    name: "Simon Hessel",
+    email: "Ottilie_Ebert10@example.com",
+    id: "84",
+  },
+  {
+    name: "Ervin Marvin",
+    email: "Catalina80@example.net",
+    id: "85",
+  },
+  {
+    name: "Perry Wisozk",
+    email: "Hailey.Littel@example.com",
+    id: "86",
+  },
+  {
+    name: "Richard Lueilwitz",
+    email: "Adriel_Cremin@example.com",
+    id: "87",
+  },
+  {
+    name: "Henrietta Hammes",
+    email: "Annabelle_Hickle50@example.com",
+    id: "88",
+  },
+  {
+    name: "Jeremiah Haley",
+    email: "Hans_Heaney30@example.org",
+    id: "89",
+  },
+  {
+    name: "Marian Hickle",
+    email: "Adalberto81@example.net",
+    id: "90",
+  },
+  {
+    name: "Lorenzo Cronin",
+    email: "Marcelina39@example.com",
+    id: "91",
+  },
+  {
+    name: "Ms. Jeremy Hodkiewicz",
+    email: "Ernestine.Reichel55@example.org",
+    id: "92",
+  },
+  {
+    name: "Leo Beatty",
+    email: "Wiley.Schumm67@example.com",
+    id: "93",
+  },
+  {
+    name: "Glen Casper",
+    email: "Chadrick.Jacobson@example.net",
+    id: "94",
+  },
+  {
+    name: "Eleanor Walter",
+    email: "Miracle.Bartell@example.com",
+    id: "95",
+  },
+  {
+    name: "Keith Pouros",
+    email: "Gracie_Hackett4@example.com",
+    id: "96",
+  },
+  {
+    name: "Alan Haag MD",
+    email: "Dorothea.Rosenbaum47@example.org",
+    id: "97",
+  },
+  {
+    name: "Miss Tracy Welch IV",
+    email: "Ashton_Weber@example.net",
+    id: "98",
+  },
+  {
+    name: "Ms. Robert Haley MD",
+    email: "Rey_Bailey63@example.org",
+    id: "99",
+  },
+  {
+    name: "Ellis Kling III",
+    email: "Ramon.Hayes@example.org",
+    id: "100",
+  },
+];
 
-export {data}
+export { data };
--- script.js
+++ script.js
@@ -1,74 +1,73 @@
-    import {data } from './data.js'
+import { data } from "./data.js";
 
+let currentPage = 1;
+let totalPage = 10;
 
+function num() {
+  return Math.ceil(data.length / totalPage);
+}
 
-    let currentPage=1;
-    let totalPage=10;
+function prev() {
+  if (currentPage > 1) {
+    currentPage--;
+    change(currentPage);
+  }
+}
 
-    function num(){
-        return Math.ceil(data.length/totalPage)
-    }
+function next() {
+  if (currentPage < num()) {
+    currentPage++;
+    change(currentPage);
+  }
+}
 
-    function prev(){
-        if(currentPage>1){
-            currentPage--;
-            change(currentPage);
-        }
-    }
+function change(page) {
+  let btnPrev = document.getElementById("prev");
+  let btnNext = document.getElementById("next");
+  let listingTable = document.getElementById("table-data");
+  let pageSpan = document.getElementById("page");
+  if (page < 1) {
+    page = 1;
+  }
+  if (page > num()) {
+    page = num();
+  }
+  listingTable.innerHTML = "";
 
-    function next(){
-        if(currentPage<num()){
-            currentPage++;
-            change(currentPage);
-        }
-    }
-
-
-    function change(page){
-        let btnPrev=document.getElementById("prev");
-        let btnNext=document.getElementById("next");
-        let listingTable=document.getElementById("table-data")
-        let pageSpan=document.getElementById('page');
-        if(page<1){
-            page=1;
-        }
-        if(page>num()){
-            page=num();
-        }
-        listingTable.innerHTML="";
-
-        for(let i=(page-1)*totalPage;i<(page*totalPage)&&i<data.length;i++){
-            listingTable.innerHTML+=`
+  for (
+    let i = (page - 1) * totalPage;
+    i < page * totalPage && i < data.length;
+    i++
+  ) {
+    listingTable.innerHTML += `
             <tr>
       <th scope="row">${data[i].id}</th>
       <td>${data[i].name}</td>
       <td>${data[i].email}</td>
-      </tr>`
-        }
-        // pageSpan.innerHTML=`${page}/${num()}`;
+      </tr>`;
+  }
+  // pageSpan.innerHTML=`${page}/${num()}`;
 
-        btnPrev.style.display=(page===1)?"none":"inline-block";
-        btnNext.style.display=(page===num())?"none":"inline-block";
+  btnPrev.style.display = page === 1 ? "none" : "inline-block";
+  btnNext.style.display = page === num() ? "none" : "inline-block";
+}
 
-    }
+window.onload = () => {
+  document.getElementById("prev").addEventListener("click", (e) => {
+    e.preventDefault();
+    prev();
+  });
+  document.getElementById("next").addEventListener("click", (e) => {
+    e.preventDefault();
+    next();
+  });
+  change(1);
+};
 
-   
-      window.onload=()=>{
-        document.getElementById("prev").addEventListener("click",(e)=>{
-            e.preventDefault();
-            prev();
-        })
-        document.getElementById("next").addEventListener("click",(e)=>{e.preventDefault();next();})
-        change(1);
-      };
-
-      let bt=document.getElementsByClassName("bt");
-      for(let i=0;i<bt.length;i++){
-        bt[i].addEventListener("click",(e)=>{
-            e.preventDefault();
-            change(parseInt(e.target.innerHTML))
-        });
-      }
-      
-      
-      
+let bt = document.getElementsByClassName("bt");
+for (let i = 0; i < bt.length; i++) {
+  bt[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    change(parseInt(e.target.innerHTML));
+  });
+}
